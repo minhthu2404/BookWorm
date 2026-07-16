c694e7782fe6ee527b192af2a7de36bc3474bd06
Trần Minh Thư <thub2303851@student.ctu.edu.vn>
2026-07-16 20:04:29 +0700

    Cài đặt cors và thư viện axios

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,15 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
