ed3dd8b1d8043077c38fc8f3834e5af40a1e6b65
Trần Minh Thư <thub2303851@student.ctu.edu.vn>
2026-07-21 14:56:40 +0700

    Xóa tất cả chức năng về Tác Giả

--- server/src/routes/tacgia.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const tacGiaController = require("../controllers/tacgia.controller");
-const router = express.Router();
-
-router.get("/", tacGiaController.findAll);
-
-module.exports = router;
