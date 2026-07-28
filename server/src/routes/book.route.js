e7059487cc0b9e7c1765bbfcf4b529206cce023a
Trần Minh Thư <thub2303851@student.ctu.edu.vn>
2026-07-28 13:27:41 +0700

    Update book.route.js

--- server/src/routes/book.route.js
+++ server/src/routes/book.route.js
@@ -3,6 +3,14 @@ const bookController = require("../controllers/book.controller");
 const router = express.Router();
 
 router.get("/", bookController.findAll);
+router.get("/count", bookController.countAll);
+router.get("/newbooks", bookController.getNewBooks);
+router.get("/related", bookController.getRelatedBooks);
+router.get("/categories", bookController.getCategories);
 router.get("/:id", bookController.findOne);
+
+router.post("/", bookController.create);
+router.put("/:id", bookController.update);
+router.delete("/:id", bookController.delete);
 
 module.exports = router;
