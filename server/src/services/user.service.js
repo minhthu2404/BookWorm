6a64f7402fb87c814baf2d2ec92123460da9cb5d
Trần Minh Thư <thub2303851@student.ctu.edu.vn>
2026-08-05 00:41:39 +0700

    Hiệu chỉnh lỗi xóa người dùng (xóa cả đơn mượn, yêu cầu liên quan)

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -2,7 +2,14 @@ const { ObjectId } = require("mongodb");
 
 class UserService {
     constructor(client) {
+        this.client = client;
         this.User = client.db().collection("NGUOIDUNG");
+        this.Cart = client.db().collection("GIOHANG");
+        this.CartDetail = client.db().collection("CHITIET_GIOHANG");
+        this.Request = client.db().collection("YEUCAU");
+        this.RequestDetail = client.db().collection("CHITIET_YEUCAU");
+        this.Ledger = client.db().collection("DONMUON");
+        this.LedgerDetail = client.db().collection("CHITIET_DONMUON");
     }
 
     // Xử lý dữ liệu
@@ -59,6 +66,30 @@ class UserService {
         }else if(!isNaN(id)){
             queryId = Number(id);
         }
+
+        // Xóa giỏ hàng và chi tiết giỏ hàng
+        const cart = await this.Cart.findOne({ MaND: queryId });
+        if (cart) {
+            await this.CartDetail.deleteMany({ MaGH: cart._id });
+            await this.Cart.deleteOne({ _id: cart._id });
+        }
+
+        // Xóa yêu cầu và chi tiết yêu cầu
+        const requests = await this.Request.find({ MaND: queryId }).toArray();
+        const requestIds = requests.map(r => r._id);
+        if (requestIds.length > 0) {
+            await this.RequestDetail.deleteMany({ MaYC: { $in: requestIds } });
+            await this.Request.deleteMany({ MaND: queryId });
+        }
+
+        // Xóa đơn mượn và chi tiết đơn mượn
+        const ledgers = await this.Ledger.find({ MaND: queryId }).toArray();
+        const ledgerIds = ledgers.map(l => l._id);
+        if (ledgerIds.length > 0) {
+            await this.LedgerDetail.deleteMany({ MaDM: { $in: ledgerIds } });
+            await this.Ledger.deleteMany({ MaND: queryId });
+        }
+
         const result = await this.User.findOneAndDelete({ _id: queryId });
         return result;
     }
